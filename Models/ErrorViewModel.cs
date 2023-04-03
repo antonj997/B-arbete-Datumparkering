8ca531525f60116b4a919e8aeb473f665b30576c
Anton Jonsson <anjo2119@student.miun.se>
2023-04-03 14:44:47 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Datumparkering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
