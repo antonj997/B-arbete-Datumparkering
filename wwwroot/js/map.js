ce4734e3fe86be28cabaee16c5ab5b0479dde786
Anton Jonsson <anjo2119@student.miun.se>
2023-04-04 10:19:18 +0200

    Map now gets users current location

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,7 +1,31 @@
 ﻿function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 63.1766832, lng: 14.636068099999989 },
-        zoom: 15,
+        zoom: 20,
         mapId: '869fd3c6510ec622'
     });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'Your Location'
+            });
+
+            map.setCenter(pos);
+        }, function () {
+            // If geolocation is not enabled, default to center of map
+            map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
+        });
+    } else {
+        // If geolocation is not supported, default to center of map
+        map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
+    }
 }
+
+
