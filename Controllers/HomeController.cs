66e4264af1381d5669d30c6a74a70a60a211e3db
calu2103 <cap_pe@hotmail.com>
2023-04-04 11:02:46 +0200

    push

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,13 +13,41 @@ namespace Datumparkering.Controllers
             _logger = logger;
         }
 
-            public DateTime GetTodaysDate()
+        public DateTime GetTodaysDate()
+        {
+            return DateTime.Now.Date;
+        }
+        public bool IsTodayDateEven()
+        {
+            DateTime today = GetTodaysDate();
+            int day = today.Day;
+            return day % 2 == 0;
+        }
+        public bool IsParkingAllowed()
+        {
+            DateTime now = DateTime.Now;
+            if (now.Month >= 10 || now.Month < 6)
             {
-                return DateTime.Now.Date;
+                if (now.Month == 10 && now.Day == 1 && now.Hour < 7)
+                {
+                    return false;
+                }
+                else if (now.Month == 5 && now.Day == 31 && now.Hour >= 7)
+                {
+                    return false;
+                }
+                else if (now.Hour >= 0 && now.Hour < 7)
+                {
+                    return true;
+                }
             }
+            return false;
+        }
         public IActionResult Index()
         {
             ViewBag.TodaysDate = GetTodaysDate();
+            bool EvanDay = IsTodayDateEven();
+            bool isParkingAllowed = IsParkingAllowed();
             return View();
         }
 
