aa6460cd47bdd3d3f0dcfb66dbd9096239f40ad6
Anton Jonsson <anjo2119@student.miun.se>
2023-04-04 13:47:45 +0200

    testing live tracking

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,49 +1,59 @@
 ﻿function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 63.1766832, lng: 14.636068099999989 },
         zoom: 20,
-        mapTypeId: 'roadmap',
-        mapId: '869fd3c6510ec622',
-        disableDefaultUI: true
+        mapTypeId: "roadmap",
+        mapId: "869fd3c6510ec622",
+        disableDefaultUI: true,
     });
 
+    // Define the geocoder
+    var geocoder = new google.maps.Geocoder();
+
+    // Define the marker
+    var marker = new google.maps.Marker({
+        map: map,
+        title: "Your Location",
+    });
+
+    // Define the info window
+    var infoWindow = new google.maps.InfoWindow();
+
+    // Use watchPosition to track the user's location
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+        navigator.geolocation.watchPosition(
+            function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
 
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                title: 'Your Location'
-            });
+                // Update the marker position
+                marker.setPosition(pos);
 
-            map.setCenter(pos);
+                // Update the map center
+                map.setCenter(pos);
 
-            // Define the current location
-            var currentLocation = new google.maps.LatLng(pos.lat, pos.lng);
-
-            // Define the geocoder
-            var geocoder = new google.maps.Geocoder();
-
-            // Find the closest address to the current location
-            geocoder.geocode({ location: currentLocation }, function (results, status) {
-                if (status === "OK") {
-                    if (results[0]) {
-                        console.log(results[0].formatted_address);
+                // Find the closest address to the current location
+                geocoder.geocode({ location: pos }, function (results, status) {
+                    if (status === "OK") {
+                        if (results[0]) {
+                            // Update the info window content
+                            infoWindow.setContent(results[0].formatted_address);
+                            infoWindow.open(map, marker);
+                        } else {
+                            console.log("No results found");
+                        }
                     } else {
-                        console.log("No results found");
+                        console.log("Geocoder failed due to: " + status);
                     }
-                } else {
-                    console.log("Geocoder failed due to: " + status);
-                }
-            });
-        }, function () {
-            // If geolocation is not enabled, default to center of map
-            map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
-        });
+                });
+            },
+            function () {
+                // If geolocation is not enabled, default to center of map
+                map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
+            }
+        );
     } else {
         // If geolocation is not supported, default to center of map
         map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
