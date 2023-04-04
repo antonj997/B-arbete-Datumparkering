37fc59b429ed9a059e309fa18088134960318d55
Anton Jonsson <anjo2119@student.miun.se>
2023-04-04 11:03:25 +0200

    Getting nearest adress to location

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -3,9 +3,10 @@
         center: { lat: 63.1766832, lng: 14.636068099999989 },
         zoom: 20,
         mapTypeId: 'roadmap',
-        mapId: '869fd3c6510ec622'
-        
+        mapId: '869fd3c6510ec622',
+        disableDefaultUI: true
     });
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
@@ -20,6 +21,25 @@
             });
 
             map.setCenter(pos);
+
+            // Define the current location
+            var currentLocation = new google.maps.LatLng(pos.lat, pos.lng);
+
+            // Define the geocoder
+            var geocoder = new google.maps.Geocoder();
+
+            // Find the closest address to the current location
+            geocoder.geocode({ location: currentLocation }, function (results, status) {
+                if (status === "OK") {
+                    if (results[0]) {
+                        console.log(results[0].formatted_address);
+                    } else {
+                        console.log("No results found");
+                    }
+                } else {
+                    console.log("Geocoder failed due to: " + status);
+                }
+            });
         }, function () {
             // If geolocation is not enabled, default to center of map
             map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
@@ -29,5 +49,3 @@
         map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
     }
 }
-
-
