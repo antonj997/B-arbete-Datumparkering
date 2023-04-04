fba9f9f58e47db12aa857d66c6be53d1275413a7
calu2103 <cap_pe@hotmail.com>
2023-04-04 11:03:19 +0200

    save

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,31 +23,11 @@ namespace Datumparkering.Controllers
             int day = today.Day;
             return day % 2 == 0;
         }
-        public bool IsParkingAllowed()
-        {
-            DateTime now = DateTime.Now;
-            if (now.Month >= 10 || now.Month < 6)
-            {
-                if (now.Month == 10 && now.Day == 1 && now.Hour < 7)
-                {
-                    return false;
-                }
-                else if (now.Month == 5 && now.Day == 31 && now.Hour >= 7)
-                {
-                    return false;
-                }
-                else if (now.Hour >= 0 && now.Hour < 7)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+   
         public IActionResult Index()
         {
             ViewBag.TodaysDate = GetTodaysDate();
             bool EvanDay = IsTodayDateEven();
-            bool isParkingAllowed = IsParkingAllowed();
             return View();
         }
 
