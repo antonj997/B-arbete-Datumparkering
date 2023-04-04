db58e22dba3fcb8de7acd2e3f77e4bd6bca74db6
Anton Jonsson <anjo2119@student.miun.se>
2023-04-04 09:56:30 +0200

    Map id applied

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,6 +1,7 @@
 ﻿function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 63.1766832, lng: 14.636068099999989 },
-        zoom: 15
+        zoom: 15,
+        mapId: '869fd3c6510ec622'
     });
 }
