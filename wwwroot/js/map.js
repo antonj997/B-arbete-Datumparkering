b5955c898683e607aac14834af102ede972f4ff0
Anton Jonsson <anjo2119@student.miun.se>
2023-04-05 11:56:16 +0200

    telemetry data

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -47,3 +47,27 @@
         map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
     }, { enableHighAccuracy: true, maximumAge: 3000 });
 }
+
+var reqcount = 0;
+
+navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+
+function successCallback(position) {
+    const { accuracy, latitude, longitude, altitude, heading, speed } = position.coords;
+    // Show a map centered at latitude / longitude.
+    reqcount++;
+    details.innerHTML = "Accuracy: " + accuracy + "<br>";
+    details.innerHTML += "Latitude: " + latitude + " | Longitude: " + longitude + "<br>";
+    details.innerHTML += "Altitude: " + altitude + "<br>";
+    details.innerHTML += "Heading: " + heading + "<br>";
+    details.innerHTML += "Speed: " + speed + "<br>";
+    details.innerHTML += "reqcount: " + reqcount;
+}
+function errorCallback(error) {
+
+}
+var options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+};
