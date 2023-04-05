20f4ff88763a3f0584ee54d8cebbeb691eee6bb8
Anton Jonsson <anjo2119@student.miun.se>
2023-04-05 09:57:29 +0200

    testing updates

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -7,52 +7,43 @@
         disableDefaultUI: true
     });
 
+    // Define the marker
+    var marker = new google.maps.Marker({
+        map: map,
+        title: "Closest address"
+    });
+
     // Define the geocoder
     var geocoder = new google.maps.Geocoder();
 
-    // Track the user's location and update the map as the user moves
-    if (navigator.geolocation) {
-        var currentLocationMarker = null;
-        var currentLocation = null;
-        navigator.geolocation.watchPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+    // Watch for location changes
+    navigator.geolocation.watchPosition(function (position) {
+        // Update marker position
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
 
-            // Update the current location marker
-            if (!currentLocationMarker) {
-                currentLocationMarker = new google.maps.Marker({
-                    position: pos,
-                    map: map,
-                    title: 'Your Location'
-                });
-            } else {
-                currentLocationMarker.setPosition(pos);
-            }
+        marker.setPosition(pos);
+        console.log("update");
 
-            // Update the current location and find the closest address
-            currentLocation = new google.maps.LatLng(pos.lat, pos.lng);
-            geocoder.geocode({ location: currentLocation }, function (results, status) {
-                if (status === "OK") {
-                    if (results[0]) {
-                        console.log(results[0].formatted_address);
-                    } else {
-                        console.log("No results found");
-                    }
+        // Find the closest address to the current location
+        geocoder.geocode({ location: pos }, function (results, status) {
+            if (status === "OK") {
+                if (results[0]) {
+                    marker.setTitle(results[0].formatted_address);
                 } else {
-                    console.log("Geocoder failed due to: " + status);
+                    marker.setTitle("No results found");
                 }
-            });
+            } else {
+                marker.setTitle("Geocoder failed due to: " + status);
+            }
+        });
 
-            // Center the map on the current location
-            map.setCenter(pos);
-        }, function () {
-            // If geolocation is not enabled, default to center of map
-            map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
-        }, { enableHighAccuracy: true, maximumAge: 3000, timeout: 5000 });
-    } else {
-        // If geolocation is not supported, default to center of map
+        // Center the map over the marker
+        map.setCenter(pos);
+    }, function () {
+        // If geolocation is not enabled, default to center of map
         map.setCenter({ lat: 63.1766832, lng: 14.636068099999989 });
-    }
+    }, { enableHighAccuracy: true, maximumAge: 3000 });
 }
